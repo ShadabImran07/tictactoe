565e77899935d353eaf4abb3a7d4728f897a9635
Shadabimran07 <md.shadabimran.aiml25@heritageit.edu.in>
2022-07-24 18:05:23 +0530

    learn Conditional expression

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Board from './components/Board';
 import Square from './components/Square';
 import { calculateWinner } from './helpers';
+import History from './components/History';
+import StatusMessage from './components/StatusMessage';
 
 import './styles/root.scss';
 const App = () => {
@@ -13,9 +15,6 @@ const App = () => {
   const current = history[currentMove];
 
   const winner = calculateWinner(current.board);
-  const message = winner
-    ? `Winner is ${winner}`
-    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
 
   const handleSquareClick = postition => {
     if (current.board[postition] || winner) {
@@ -36,11 +35,15 @@ const App = () => {
 
     setCurrentMove(prev => prev + 1);
   };
+  const moveTo = move => {
+    setCurrentMove(move);
+  };
   return (
     <div className="app">
       <h1>TIC TAC TOE </h1>
-      <p>{message}</p>
+      <StatusMessage winner={winner} current={current} />
       <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
